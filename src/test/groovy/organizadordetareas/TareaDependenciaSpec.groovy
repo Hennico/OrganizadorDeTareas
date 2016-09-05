8d7f532e54618bb17c51bb45a12b91bc7f996689
Hennico <nicolas_zanardo@hotmail.com>
2016-09-05 18:41:27 -0300

    Update and rename TareaRelacionSpec.groovy to TareaDependenciaSpec.groovy

--- src/test/groovy/organizadordetareas/TareaDependenciaSpec.groovy
+++ src/test/groovy/organizadordetareas/TareaDependenciaSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(TareaRelacion)
-class TareaRelacionSpec extends Specification {
+@TestFor(TareaDependencia)
+class TareaDependencia extends Specification {
 
     def setup() {
     }
