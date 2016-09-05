f53b6cfdecd4468acc66e708f4e841687eac8d9a
Hennico <nicolas_zanardo@hotmail.com>
2016-09-05 18:15:47 -0300

    Update Usuario.groovy

--- grails-app/domain/organizadordetareas/Usuario.groovy
+++ grails-app/domain/organizadordetareas/Usuario.groovy
@@ -1,14 +1,14 @@
 package organizadordetareas
 
 class Usuario {
-    Set<EstadoTarea> tareasAsignadas
+    Set<RealacionUsuarioTarea> tareasAsignadas
 
     static constraints = {
     }
 
     void AsignarTarea(Tarea nuevaTarea, UsuarioRol rol) {
         if (!tareasAsignadas.any({ tareaActual -> tareaActual.tarea == nuevaTarea }))
-            tareasAsignadas.add(new EstadoTarea([
+            tareasAsignadas.add(new RealacionUsuarioTarea([
                 tarea: nuevaTarea,
                 estado: Estado.pendiente,
                 usuarioRol: rol,
